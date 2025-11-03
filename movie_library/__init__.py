535f7749182979b88ada65092f0129929c7ee01d
MRPeops01 <168998335+MRPeops01@users.noreply.github.com>
2025-11-03 13:39:40 -0500

    Implement test user creation in create_app
    Add a test user to the database if not present.

--- movie_library/__init__.py
+++ movie_library/__init__.py
@@ -2,11 +2,10 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from pymongo import MongoClient
-
+from werkzeug.security import generate_password_hash
 from movie_library.routes import pages
 
 load_dotenv()
-
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +14,15 @@ def create_app():
         "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
     )
     app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
+
+    # Add test user ONLY if not already in DB
+    if not app.db.users.find_one({"email": "testuser@example.com"}):
+        app.db.users.insert_one({
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password": generate_password_hash("password123")
+        })
+        print("Test user added to Atlas!")
 
     app.register_blueprint(pages)
     return app
